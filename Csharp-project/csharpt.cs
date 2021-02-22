55b21eb92fd07b083af395be56a18b82a46bfcd5
hoartist <ddongsa123@naver.com>
2021-02-22 23:33:05 +0900

    backjoon

---
+++ Csharp-project/csharpt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Csharpproject
+{
+    class Mathematics
+    {
+        public int f(int x)
+        {
+            return x * x;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Mathematics m = new Mathematics();
+            int result = m.f(5);
+            Console.WriteLine(result);
+        }
+        }
+    }
